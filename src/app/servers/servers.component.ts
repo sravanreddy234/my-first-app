3721a1551724ded0f746434af87c226b5f551abb
sravan reddy <sravanreddy234@gmail.com>
2022-01-27 15:20:36 +0530

    Directives
    ngIf directive (If and else)

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,15 +9,16 @@ export class ServersComponent implements OnInit {
     allowNewServer = false;
     serverCreatingStatus = "No Server is created";
     serverName = "test server";
-    
+    serverCreated = false;
   constructor() { 
     setTimeout(() => {
-      this.allowNewServer = true;
+      this.allowNewServer = false;
     }, 2000);
   }
   ngOnInit(): void {
   }
   onCreateServer(){
+    this.serverCreated = true;
     this.serverCreatingStatus = "Server is Created "+this.serverName;
   }
 
